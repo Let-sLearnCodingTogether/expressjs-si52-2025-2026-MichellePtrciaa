3495cb897b60b628b2df89f1319f41ac8cb7b77e
MichellePtrciaa <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-09-24 20:38:15 +0700

    pert2

--- routes/web.js
+++ routes/web.js
@@ -6,7 +6,15 @@ web.get('/',(req, res) =>{
     res.render('index')
 })
 web.get('/:username', (req,res)=>{
-    res.render("public-profile")
+    const username = req.params.username;
+    res.render("public-profile", {
+        title : username,
+        username: username,
+        bio: "semangat"
+    })
 })
+
+
+
 
 export default web
